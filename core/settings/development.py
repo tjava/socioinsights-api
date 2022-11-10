f78067e6249ad313b661da60ec3eb3a3f0cf522d
tjava <mustyytee@gmail.com>
2022-11-10 21:24:36 +0100

    project setup

---
+++ core/settings/development.py
@@ -0,0 +1,21 @@
+from .base import *
+
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = "info@socioinsights.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Socio Insights"
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": env("POSTGRES_ENGINE"),
+#         "NAME": env("POSTGRES_DB"),
+#         "USER": env("POSTGRES_USER"),
+#         "PASSWORD": env("POSTGRES_PASSWORD"),
+#         "HOST": env("PG_HOST"),
+#         "PORT": env("PG_PORT"),
+#     }
+# }
